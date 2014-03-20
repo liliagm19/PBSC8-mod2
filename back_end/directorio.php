9f269755990c3b431dee92ab6d17d49b584f7081
liliagm19 <lilia_gm19@hotmail.com>
2014-03-20 17:33:05 -0600

    Actualizacion

--- back_end/directorio.php
+++ back_end/directorio.php
@@ -1,5 +1,5 @@
 <?php
-	$url = "http://localhost/drupalsite/?q=webservicesphp/lista";
+	$url = "http://192.168.0.10/drupalsite/?q=webservicesphp/lista";
 
         $options = array(
 		'method' => 'POST',
@@ -7,6 +7,21 @@
 	);
 	$context = stream_context_create();
 	$result = file_get_contents($url, false, $context);
-	echo $result;
 
+	$dom = new domDocument;
+	$dom->loadHTML($result);
+	$dom->preserveWhiteSpace = false;
+	$tables = $dom->getElementsByTagName('table');
+	$rows = $tables->item(0)->getElementsByTagName('tr');
+	echo '<h2> Ejemplo de cómo parsear y dar formato a los campos del archivo</h2>';
+	echo '<table>';
+	echo '<col width="150">';
+	echo '<col width="500">';
+	foreach($rows as $row)
+	{
+		$cols = $row->getElementsByTagName('td');
+		echo '<tr><td><b>'.$cols->item(1)->nodeValue.'</b></td>';
+		echo '<td><i><font color="red">'.$cols->item(0)->nodeValue.'</font></i></td></tr>';
+	}
+	echo '</table>';
 ?>
